acef5aad1d943d063248d78b0d94ad5193fb252d
bnnlukas <bonn_lukas@gmx.de>
2023-07-30 13:07:29 +0200

    Current Version added

---
+++ config.py
@@ -0,0 +1,27 @@
+# Source: https://github.com/Dschobby/flappy_bird_reinforcement_learning/blob/main/config.py
+"""
+Game constants
+"""
+
+#Pygame window
+SCREEN_WIDHT = 400
+SCREEN_HEIGHT = 600
+
+#Physics
+SPEED = 15
+GRAVITY = 2
+GAME_SPEED = 10
+
+#Ground
+GROUND_WIDHT = 2 * SCREEN_WIDHT
+GROUND_HEIGHT= 100
+
+#Pipe
+PIPE_WIDHT = 70
+PIPE_HEIGHT = 500
+PIPE_GAP = 150
+PIPE_DISTANCE = 280
+
+#Bird
+BIRD_WIDTH = 40
+BIRD_HEIGHT = 30
